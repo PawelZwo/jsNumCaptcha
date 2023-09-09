bd30c32dd01eda6e69b2843471278c9ec584d887
Paweł Zwoliński <pawel_zwolinski@icloud.com>
2023-09-09 16:32:42 +0200

    captcha working properly

--- index.js
+++ index.js
@@ -2,49 +2,43 @@ const captchaQuestion = document.getElementById("captchaQuestion");
 const form = document.getElementById("captchaForm");
 const formInput = document.getElementById("captchaAnswer");
 const newCaptchaButton = document.getElementById("newCaptcha");
-// const captchaFormButton = document.getElementById("captchaFormButton");
+const captchaFormButton = document.getElementById("captchaFormButton");
 
-const options = ["+", "-", "*"];
+let captchaAnswer;
 
 const generateCaptcha = () => {
+  captchaFormButton.disabled = false;
+
+  const options = ["+", "-", "*"];
   let randomOption = Math.floor(Math.random() * options.length),
     randomA = Math.floor(Math.random() * (10 - 1) + 1),
-    randomB = Math.floor(Math.random() * (10 - 1) + 1),
-    captchaText = `${randomA} ${options[randomOption]} ${randomB} = ?`,
-    captchaAnswer;
+    randomB = Math.floor(Math.random() * (10 - 1) + 1)
+
+  captchaQuestion.textContent = `${randomA} ${options[randomOption]} ${randomB} = ?`;
 
   switch (options[randomOption]) {
     case "+":
-      captchaAnswer = randomA + randomB;
-      break;
+      return captchaAnswer = randomA + randomB;
     case "-":
-      captchaAnswer = randomA - randomB;
-      break;
+      return captchaAnswer = randomA - randomB;
     case "*":
-      captchaAnswer = randomA * randomB;
-      break;
+      return captchaAnswer = randomA * randomB;
   }
-
-  console.log(`captchaAnswer: ${captchaAnswer}, captchaText: ${captchaText}`)
-  captchaQuestion.textContent = captchaText;
-  return captchaAnswer
 }
 
 document.onload = generateCaptcha();
 
+newCaptchaButton.addEventListener("click", () => generateCaptcha())
 
-newCaptchaButton.addEventListener("click", e => {
-  e.preventDefault();
-  generateCaptcha();
-})
-
-form.addEventListener("submit", (e, captchaAnswer) => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
   if (Number(formInput.value) !== Number(captchaAnswer)) {
     console.error("Bad answer!");
     formInput.value = "";
     generateCaptcha();
+  } else {
+    console.log("Good answer! Sending your request to the server...")
+    formInput.value = "";
+    captchaFormButton.disabled = true;
   }
-  console.log("Good answer! Sending your request to the server...")
-  formInput.value = "";
 })
